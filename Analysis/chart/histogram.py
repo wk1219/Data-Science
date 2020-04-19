82d39bb2a6c12ec58c199bdff075deb1942f58c6
wk1219 <sjms1215@naver.com>
2020-04-19 23:20:28 +0900

    study histogram

---
+++ Analysis/chart/histogram.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+from collections import Counter
+
+grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0, 112, 120]
+histogram = Counter(min(grade // 10 * 10, 110) for grade in grades)
+
+plt.bar([x + 5 for x in histogram.keys()],
+        histogram.values(), 10, edgecolor=(0, 0, 0))
+
+plt.axis([-5, 125, 0, 5])
+
+plt.xticks([10 * i for i in range(13)])
+plt.xlabel("Decile")
+plt.ylabel("# of Students")
+plt.title("Distribution of Exam 1 Grades")
+plt.show()
+plt.savefig('../im/viz_grades.png')
+plt.gca().clear()
