df9998b9785103fdb806882d14526c69ada1cb6c
wk1219 <sjms1215@naver.com>
2020-04-20 23:43:38 +0900

    study scatter charts

--- Analysis/chart/scatter_charts.py
+++ Analysis/chart/scatter_charts.py
@@ -9,6 +9,8 @@ plt.scatter(friends, minutes)
 for label, friend_cnt, minute_cnt in zip(labels, friends, minutes):
     plt.annotate(label, xy=(friend_cnt, minute_cnt), xytext=(10, -10), textcoords='offset points')
 
+plt.annotate('annotate', xy=(72, 205), xytext=(66, 170), fontsize=8,
+             arrowprops=dict(facecolor='green', edgecolor='green', headlength=5))
 plt.title("Daily Minutes vs. Number of Friends")
 plt.xlabel("# of friends")
 plt.ylabel("daily minutes spent on the site")
