873fae6baebf42d7c76d1593f7f899ac744d342f
wk1219 <sjms1215@naver.com>
2020-06-10 21:03:17 +0900

    Add mini-batch-learning.py

---
+++ AI/mini-batch-learning.py
@@ -0,0 +1,30 @@
+import numpy as np
+from dataset.mnist import load_mnist
+from Training.Twolayer_nn import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+
+# Hyper Parameter
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+
