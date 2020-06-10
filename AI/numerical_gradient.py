072ff7c616a044553229140cbb75f7646145bc7d
wk1219 <sjms1215@naver.com>
2020-06-10 19:42:37 +0900

    Add numerical_gradient.py

---
+++ AI/numerical_gradient.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+def numeriacal_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+print(numeriacal_gradient(function_2, np.array([3.0, 4.0])))
+print(numeriacal_gradient(function_2, np.array([0.0, 2.0])))
+print(numeriacal_gradient(function_2, np.array([3.0, 0.0])))
