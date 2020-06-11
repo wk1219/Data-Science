7b821de91be68628884736c4e63ff4a1d17a009e
wk1219 <sjms1215@naver.com>
2020-06-11 14:24:56 +0900

    Add AffineLayer Softmax-with-LossLayer.py

---
+++ AI/Back-Propagation/Softmax-with-LossLayer.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def softmax(a):
+    c = np.max(a)   # Overflow Measures
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
