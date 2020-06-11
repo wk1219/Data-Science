7b4077edf8afc6cc422a304ab1e48cad02072335
wk1219 <sjms1215@naver.com>
2020-06-11 16:52:06 +0900

    Modify BP-trainig.py

--- AI/Back-Propagation/BP-training.py
+++ AI/Back-Propagation/BP-training.py
@@ -128,7 +128,7 @@ class TwoLayerNet:
         grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
         grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
         grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
         return grads
 
     def gradient(self, x, t):
